447d4cf2e427ae6aec187d19d694ef159cdf063b
Raj Aththanayake <rajitha_kba@yahoo.com.au>
2011-10-06 06:45:04 +1100

    Add public API comments to UnityAutoMoqExtensions

--- src/UnityAutoMoq/UnityAutoMoqExtension.cs
+++ src/UnityAutoMoq/UnityAutoMoqExtension.cs
@@ -10,11 +10,18 @@ namespace UnityAutoMoq
     {
         private readonly UnityAutoMoqContainer autoMoqContainer;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnityAutoMoqExtension"/> class.
+        /// </summary>
+        /// <param name="autoMoqContainer">The auto moq container.</param>
         public UnityAutoMoqExtension(UnityAutoMoqContainer autoMoqContainer)
         {
             this.autoMoqContainer = autoMoqContainer;
         }
 
+        /// <summary>
+        /// Initialize the <see cref="UnityAutoMoqExtension"/>.
+        /// </summary>
         protected override void Initialize()
         {
             Context.Strategies.Add(new UnityAutoMoqBuilderStrategy(autoMoqContainer), UnityBuildStage.PreCreation);
